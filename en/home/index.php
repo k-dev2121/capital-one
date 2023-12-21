1d201165abacf4c762023d9f2a438e1beb12c4ac
Kamsey <kamsey11@gmail.com>
2023-12-21 11:00:46 +0100

    clone complete

---
+++ en/home/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Loading...</title>
+  <script type="text/javascript">
+    window.location='../';
+  </script>
+</head>
+<body>
+ <script type="text/javascript">
+    window.location='../';
+  </script>
+</body>
+</html>
